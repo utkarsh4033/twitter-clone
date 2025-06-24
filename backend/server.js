4b1fe0c83075c0a70cb2d8cad08b9b4c037e33b8
utkarsh112002 <usharma@academian.com>
2025-06-24 19:45:31 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,6 @@ const PORT = process.env.PORT || 5000;
 
 sequelize.sync({ alter: true }).then(() => {
   console.log('Database connected and models synced');
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
 });
 
